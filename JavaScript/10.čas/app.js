5a2a4f64e62fb5c5d5d5bb3d0ea1e73790c05dcf
muusayecirovic <muusazecirovic@gmail.com>
2022-09-12 21:04:09 +0200

    14.cas

---
+++ JavaScript/10.čas/app.js
@@ -0,0 +1,36 @@
+// NASTAVAK METODA KOD STRINGOVA
+
+// Menjanje sadrzaja se moze koristiti koriscenjem replace metode.
+
+let recenica = "nekad zelimo zameniti sadrzaj nekim drugim";
+
+let novarecenica = recenica.replace("sadrzaj", "tekst");
+
+console.log(novarecenica);
+
+let recenica2 =
+  "Nekad zelimo da se na odredjenom mestu nadje sadrzaj koji nije isti  kao pre";
+
+let novarecenica2 = recenica2.replace("sadrzaj", "tekst");
+
+console.log(novarecenica2);
+
+let recenica4 =
+  "Nekad zelimo da se na odredjenom mestu nadje sadrzaj koji nije isti  kao pre";
+
+let novarecenica5 = recenica4.replace(/sadrzaj/gi, "tekst");
+
+let = recenica6 = "sta ako zelimo da budu sva slova velika";
+
+console.log(recenica6.toUpperCase());
+
+let recenica6 = "STA AKO ZELIMO DA BUDU SVA SLOVA VELIKA";
+
+console.log(recenica6.toLowerCase());
+
+var rece1 = "Ako predjemo prvi ciklus";
+var rece2 = "nastavljamo sa oybiljnijim bavljenjem js om";
+
+rece1And2 = rece1.concat(rece2);
+
+console.log(rece1And2);
