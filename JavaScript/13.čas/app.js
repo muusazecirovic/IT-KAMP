85e0bec60556aca2c59db1caf697386e22092917
muusayecirovic <muusazecirovic@gmail.com>
2022-09-21 21:03:24 +0200

    ...

---
+++ JavaScript/13.čas/app.js
@@ -0,0 +1,52 @@
+// 1.Prebrojati koliko se puta u unetom stringu pojavljuje veliko i malo slovo "M"
+// Npr.za string 'Mama ima momu',dobija se rezultat 5.
+
+// function firsttask(){
+// let recenica = prompt("Unesite neku recenicu")
+// let duzina = recenica.length
+// let brojac=0
+
+// for (let i=0;i<duzina;i++){
+//     if (recenica[i])
+// }
+
+// }
+
+// 2.Prebrojati koliko ima znakova koji su cifre u unetom stringu
+
+// function drugizadatak() {
+//   let recenica = prompt("Unesite neku recenicu:");
+//   let duzina = recenica.length;
+//   let brojac = 0;
+
+//   for (let i = 0; i < duzina; i++) {
+//     if (
+//       (recenica[i] === "0",
+//       recenica[i] === "1",
+//       recenica[i] === "2",
+//       recenica[i] === "3",
+//       recenica[i] === "4",
+//       recenica[i] === "5",
+//       recenica[i] === "6",
+//       recenica[i] === "7",
+//       recenica[i] === "8",
+//       recenica[i] === "9")
+//     )
+//       brojac++;
+//   }
+//   return `Uneta recenica sadrzi ${brojac}cifara.`;
+// }
+
+// function trecizadatak() {
+//   let recenica = prompt("Unesite neku recenicu:");
+//   let duzina = recenica.length;
+//   let brojac = 0;
+
+//   for (let i = 0; i < duzina; i++) {
+//     if (recenica[i] === recenica[i].toLowerCase() && recenica[i] !== " ") {
+//       brojac++;
+//     }
+//   }
+//   return `U datoj recinici ima ${brojac}slova`;
+// }
+// console.log(trecizadatak());
