fd11df460e22f82bb70ba537f9243cdbc1712862
muusayecirovic <muusazecirovic@gmail.com>
2022-10-19 20:44:43 +0200

    cas30

---
+++ JavaScript/21.čas/app2.js
@@ -0,0 +1,18 @@
+// Java script sortiranje nizova
+// sort()metoda-vrsi sortiranje niza abecedno
+
+const grupa = ["Muusa", "Amina", "Abdurahman", "Ahmed", "Amina", "Mejra"];
+grupa.sort();
+console.log(grupa);
+// Za obrnuto sortiranje imamo reverse()
+
+grupa.reverse();
+
+const brojevi = [3, 4, 24, -34, -53, 324, -5335];
+brojevi.sort();
+brojevi.sort(function (a, b) {
+  return a - b;
+});
+console.log(brojevi);
+
+//
