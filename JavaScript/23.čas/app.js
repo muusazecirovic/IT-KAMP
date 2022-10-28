d4b8a781ce78581c7cae143b421f0e290c596762
muusayecirovic <muusazecirovic@gmail.com>
2022-10-28 21:02:25 +0200

    df

---
+++ JavaScript/23.čas/app.js
@@ -0,0 +1,9 @@
+// Reduce metoda vrsi iteraciju kroz svaki element s tim sto kao rezultat daje jedan broj
+// najcesca primena reduce metode je kada zelimo sumu elementa niza
+
+const korpa = [1600, 900, 1400, 220, 1490];
+
+const godine = [15, 14, 19, 18, 17, 18, 20, 13];
+// Napraviti funkciju koja vraca zbir godina koje su vece od 17.
+
+const sumAdult = (arr) => {};
