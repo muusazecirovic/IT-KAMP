8a5c67f47addfc54a2f6fb40e27c5e046b8e4659
muusayecirovic <muusazecirovic@gmail.com>
2022-11-21 20:31:05 +0100

    30

--- JavaScript/30.cas/app2.js
+++ JavaScript/30.cas/app2.js
@@ -1,1 +1,11 @@
-const person = {};
+const abdurahman = "abdurahman";
+const selmanovic = "selmanovic";
+const godine = "14";
+const skola = "bratstvo";
+
+const person = {
+  abdurahman: abdurahman,
+  selmanovic: selmanovic,
+  godine: godine,
+  skola: skola,
+};
