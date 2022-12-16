ddff9f6b8ae29a35e0e42bb073e47024d02b9767
muusayecirovic <muusazecirovic@gmail.com>
2022-12-16 20:54:21 +0100

    izm

---
+++ JavaScript/39.čas/app2.js
@@ -0,0 +1,18 @@
+// Maps
+
+// Mape pamte originalni redosled elementa
+
+// Kreiranje mape:
+const fruits = new Map([
+  ["apples", 60],
+  ["bananas", 150],
+  ["strawberrys", 320],
+  ["mango", 440],
+]);
+console.log(fruits);
+
+// *Metode:
+
+// set()-Setuje vrednost za odredjeni kljuc u mapi
+fruits.set("pear", 40);
+console.log(fruits);
