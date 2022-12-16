2e1bea8e497b7ffe7dcd815cf333261750408ac2
muusayecirovic <muusazecirovic@gmail.com>
2022-12-16 20:46:48 +0100

    ko

---
+++ JavaScript/39.čas/app.js
@@ -0,0 +1,11 @@
+// for eaach()- poziva callback funkciju za svaki element
+
+letters.forEach((element) => console.log(element));
+
+// values()vraca iterator sa vrednostima datog seta
+console.log(letters.values());
+
+// Napraviti funkciju koja iz datog niza vraca novi niz,
+// brisuci duplikate orginalom.(koriscenjem setova);
+
+const brisiduplikate = arr;
